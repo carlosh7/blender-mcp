c6b1514c7f683acebda4caae33986b7c4db4961d
carlosh7 <carlosh7@gmail.com>
2026-08-04 18:17:00 -0500

    fix: correct syntax error in mesh_engine.py
    Fixed syntax error in smooth function (missing parentheses)

--- addon/core/mesh_engine.py
+++ addon/core/mesh_engine.py
@@ -542,7 +542,7 @@ def smooth_mesh(obj, iterations=1):
         bpy.context.view_layer.objects.active = obj
         bpy.ops.object.mode_set(mode='EDIT')
         bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.mesh.smooth iterations=iterations
+        bpy.ops.mesh.smooth(iterations=iterations)
         bpy.ops.object.mode_set(mode='OBJECT')
     return obj
 
